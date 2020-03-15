aa46bd0262be9c5b2139297e7129cfcb9cc1adb8
Aaron Kinney <akinney@students.mchenry.edu>
2020-03-15 17:46:57 -0500

    commit

---
+++ ProductFinder/TableCell.swift
@@ -0,0 +1,65 @@
+//
+//  TableCell.swift
+//  ProductFinder
+//
+//  Created by Aaron Kinney on 3/14/20.
+//  Copyright © 2020 HarvardInc. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+
+    
+    
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+ 
+     var productItem: String = "" {
+         didSet {
+             if (productItem != oldValue) {
+                 productLabel.text = productItem
+
+             }
+
+         }
+
+     }
+ 
+    var productPrice: Double = 0.0 {
+        didSet {
+            if (productPrice - oldValue > 0) {
+                priceLabel.text = String(format: "$%.02f", productPrice)
+
+            }
+
+        }
+
+    }
+ 
+    var productPicture: UIImage? = UIImage(named: "corn")! {
+         didSet {
+             if productPicture != oldValue  {
+                productImage.image = productPicture
+
+             }
+
+         }
+
+     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ProductFinder/ViewController.swift
+++ ProductFinder/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+     let cellTableIdentifier = "CellTableIdentifier"
+    
+    @IBOutlet weak var tableView: UITableView!
+   
 
+    let productMaster: [PriceProductPic] = [PriceProductPic(name: "Beets", price: 3.00, pic: UIImage(named: "beet")!), PriceProductPic(name: "Carrots", price: 3.00, pic: UIImage(named: "carrot")!), PriceProductPic(name: "Corn", price: 5.00, pic: UIImage(named: "corn")!), PriceProductPic(name: "Broccoli", price: 1.00, pic: UIImage(named: "broccoli")!)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(TableCell.self, forCellReuseIdentifier: cellTableIdentifier)
+        let xib = UINib(nibName: "TableCell", bundle: nil)
+        
+        tableView.register(xib, forCellReuseIdentifier: cellTableIdentifier)
+        tableView.rowHeight = 66 }
 
+ 
+    // MARK: Table View Data Source Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         
+        return productMaster.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell( withIdentifier: cellTableIdentifier, for: indexPath) as! TableCell
+        let rowData = productMaster[indexPath.row]
+        
+        cell.productItem = rowData.productName
+        cell.productPrice = rowData.productPrice
+        cell.productPicture = rowData.productPic
+        
+        return cell
+        
+    }
+ 
 
 }
-
